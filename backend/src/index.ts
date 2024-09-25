bd10455d351fce7329b6156c77c7d057e3305468
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-25 14:27:16 +0200

    add route to get people

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+
+const app: Express = express();
+const port = 3001;
+
+app.use(express.json());
 
 type Person = {
     id: number;
@@ -8,13 +15,54 @@ type Person = {
     country: string;
 }
 
-const app: Express = express();
-const port = 3001;
+const people: Person[] = [
+	{
+		id: 1,
+		name: "Lucas Lima",
+		street: "Rua das Palmeiras",
+		city: "Santos",
+		country: "Brasil",
+	},
+	{
+		id: 2,
+		name: "Bruno Mendes",
+		street: "Avenida Central",
+		city: "Campinas",
+		country: "Brasil",
+	},
+	{
+		id: 3,
+		name: "Paula Alves",
+		street: "Rua do Porto",
+		city: "Manaus",
+		country: "Brasil",
+	},
+	{
+		id: 4,
+		name: "Felipe Nunes",
+		street: "Rua das Laranjeiras",
+		city: "Fortaleza",
+		country: "Brasil",
+	},
+	{
+		id: 5,
+		name: "Ana Borges",
+		street: "Avenida Rio Verde",
+		city: "Goiania",
+		country: "Brasil",
+	},
+];
 
-app.use(express.json());
+
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Backend with TS is running!");
+});
+
+app.get("/people", (req, res) => {
+	res
+        .status(200)
+        .json(people);
 });
 
 
