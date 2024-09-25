dfab919da7d7157a557e9fc5197c52266e7b8dc5
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-09-25 14:09:26 +0200

    create people and type person

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,17 @@
 import express, { Express, Request, Response } from "express";
 
+type Person = {
+    id: number;
+    name: string;
+    street: string;
+    city: string;
+    country: string;
+}
+
 const app: Express = express();
 const port = 3001;
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Backend with TS is running!");
